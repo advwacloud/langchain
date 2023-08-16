3a952b2e4cf50b6dd85c9fc935a987a02a4e34a6
ADVANTECH\Boshen.Chen <boshen.chen@advantech.com.tw>
2023-08-16 11:12:55 +0800

    [update] use select all in sample

--- libs/langchain/langchain/utilities/sql_database.py
+++ libs/langchain/langchain/utilities/sql_database.py
@@ -343,7 +343,9 @@ class SQLDatabase:
 
     def _get_sample_rows(self, table: Table) -> str:
         # build the select command
-        command = select(table).limit(self._sample_rows_in_table_info)
+
+        tmp = Table(table.name,self._metadata)
+        command = select('*',tmp).limit(self._sample_rows_in_table_info)
 
         # save the columns in string format
         columns_str = "\t".join([col.name for col in table.columns])
